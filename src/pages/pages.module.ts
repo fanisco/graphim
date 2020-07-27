360e4987488e7e0412a0777b518227194db49d85
fa.zubairov <fa.zubairov@tensor.ru>
2020-07-27 13:49:40 +0200

    Pages changes

--- src/pages/pages.module.ts
+++ src/pages/pages.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DynamicController } from './dynamic.controller';
 import { PagesController } from './pages.controller';
 import { PagesService } from './pages.service';
 import { Page } from './page.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Page])],
-  controllers: [PagesController],
+  controllers: [DynamicController, PagesController],
   providers: [PagesService],
 })
 export class PagesModule {}
